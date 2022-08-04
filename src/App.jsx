7c9659e65378837d6d06425f1f9128c1f5932c01
HailGanyu <henryyanguk@gmail.com>
2022-08-04 12:53:28 +0100

    git issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,176 @@
 import { useEffect } from 'react';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 function App() {
-  useEffect(() => {
-    const scene = new THREE.Scene();
+  'use strict';
 
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
-    camera.position.z = 96;
+  var scene, camera, renderer;
+  var container, HEIGHT,
+      WIDTH, fieldOfView, aspectRatio,
+      nearPlane, farPlane, stats,
+      geometry, particleCount,
+      i, h, color, size,
+      materials = [],
+      mouseX = 0,
+      mouseY = 0,
+      windowHalfX, windowHalfY, cameraZ,
+      fogHex, fogDensity, parameters = {},
+      parameterCount, particles;
 
-    const canvas = document.getElementById('myThreeJsCanvas')
-    const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true,
-    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+  init();
+  animate();
 
-    const ambientLight = new THREE.AmbientLight(0xFFC0CB, 0.5);
-    ambientLight.castShadow = true;
-    scene.add(ambientLight);
+  function init() {
+      HEIGHT = window.innerHeight;
+      WIDTH = window.innerWidth;
+      windowHalfX = WIDTH / 2;
+      windowHalfY = HEIGHT / 2;
+      fieldOfView = 75;
+      aspectRatio = WIDTH / HEIGHT;
+      nearPlane = 1;
+      farPlane = 3000;
+      cameraZ = farPlane / 3;
+      fogHex = 0x000000; /* As black as your heart.	*/
+      fogDensity = 0.0007; /* So not terribly dense?	*/
 
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(0, 64, 32);
-    scene.add(spotLight);
+      camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+      camera.position.z = cameraZ;
+      scene = new THREE.Scene();
+      scene.fog = new THREE.FogExp2(fogHex, fogDensity);
 
-    const sphereGeometry = new THREE.SphereGeometry(12, 64, 32);
-    const sphereTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/GanyuHail/3dArt/main/uniPinch1.jpg');
-    const sphereMaterial = new THREE.MeshBasicMaterial({ map: sphereTexture });
-    const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    scene.add(sphereMesh);
+      container = document.createElement('div');
+      document.body.appendChild(container);
+      document.body.style.margin = 0;
+      document.body.style.overflow = 'hidden';
 
-    sphereGeometry.userData = { URL: "https://github.com/GanyuHail/3dArt/blob/main/Hi%20Res%20-.jpg" };
+      geometry = new THREE.Geometry();
+      particleCount = 20000;
 
-    const controls = new OrbitControls(camera, renderer.domElement);
 
-    const animate = () => {
-      sphereMesh.rotation.x += 0.001;
-      sphereMesh.rotation.y += 0.001;
-      controls.update();
+      for (i = 0; i < particleCount; i++) {
+          var vertex = new THREE.Vector3();
+          vertex.x = Math.random() * 2000 - 1000;
+          vertex.y = Math.random() * 2000 - 1000;
+          vertex.z = Math.random() * 2000 - 1000;
+
+          geometry.vertices.push(vertex);
+      }
+
+      parameters = [
+          [
+              [1, 1, 0.5], 5
+          ],
+          [
+              [0.95, 1, 0.5], 4
+          ],
+          [
+              [0.90, 1, 0.5], 3
+          ],
+          [
+              [0.85, 1, 0.5], 2
+          ],
+          [
+              [0.80, 1, 0.5], 1
+          ]
+      ];
+      parameterCount = parameters.length;
+
+      for (i = 0; i < parameterCount; i++) {
+
+          color = parameters[i][0];
+          size = parameters[i][1];
+          materials[i] = new THREE.PointCloudMaterial({
+              size: size
+          });
+
+          particles = new THREE.PointCloud(geometry, materials[i]);
+          particles.rotation.x = Math.random() * 6;
+          particles.rotation.y = Math.random() * 6;
+          particles.rotation.z = Math.random() * 6;
+
+          scene.add(particles);
+      }
+
+      renderer = new THREE.WebGLRenderer();
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(WIDTH, HEIGHT);
+      container.appendChild(renderer.domElement);
+      stats = new Stats();
+      stats.domElement.style.position = 'absolute';
+      stats.domElement.style.top = '0px';
+      stats.domElement.style.right = '0px';
+      container.appendChild(stats.domElement);
+
+      window.addEventListener('resize', onWindowResize, false);
+      document.addEventListener('mousemove', onDocumentMouseMove, false);
+      document.addEventListener('touchstart', onDocumentTouchStart, false);
+      document.addEventListener('touchmove', onDocumentTouchMove, false);
+  }
+
+  function animate() {
+      requestAnimationFrame(animate);
+      render();
+      stats.update();
+  }
+
+  function render() {
+      var time = Date.now() * 0.00005;
+      camera.position.x += (mouseX - camera.position.x) * 0.05;
+      camera.position.y += (-mouseY - camera.position.y) * 0.05;
+      camera.lookAt(scene.position);
+
+      for (i = 0; i < scene.children.length; i++) {
+          var object = scene.children[i];
+          if (object instanceof THREE.PointCloud) {
+              object.rotation.y = time * (i < 4 ? i + 1 : -(i + 1));
+          }
+      }
+
+      for (i = 0; i < materials.length; i++) {
+          color = parameters[i][0];
+          h = (360 * (color[0] + time) % 360) / 360;
+          materials[i].color.setHSL(h, color[1], color[2]);
+      }
+
       renderer.render(scene, camera);
-      window.requestAnimationFrame(animate);
-    };
-    animate();
-  }, []);
+  }
+
+  function onDocumentMouseMove(e) {
+      mouseX = e.clientX - windowHalfX;
+      mouseY = e.clientY - windowHalfY;
+  }
+
+  function onDocumentTouchStart(e) {
+
+      if (e.touches.length === 1) {
+          e.preventDefault();
+          mouseX = e.touches[0].pageX - windowHalfX;
+          mouseY = e.touches[0].pageY - windowHalfY;
+      }
+  }
+
+  function onDocumentTouchMove(e) {
+
+      if (e.touches.length === 1) {
+          e.preventDefault();
+          mouseX = e.touches[0].pageX - windowHalfX;
+          mouseY = e.touches[0].pageY - windowHalfY;
+      }
+  }
+
+  function onWindowResize() {
+      windowHalfX = window.innerWidth / 2;
+      windowHalfY = window.innerHeight / 2;
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+}();
 
   return (
     <div>
       <canvas id="myThreeJsCanvas" />
     </div>
   );
-};
 
 export default App;
